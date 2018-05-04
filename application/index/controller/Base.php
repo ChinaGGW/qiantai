e17e70bdc23966070a9774e349ac4a01e6b89e23
ChinaGGW <36232080+ChinaGGW@users.noreply.github.com>
2018-05-04 09:48:00 +0800

    Update Base.php

--- application/index/controller/Base.php
+++ application/index/controller/Base.php
@@ -1,22 +1,34 @@
 <?php
 namespace app\index\controller;
+use app\index\model\UserModel;
 use think\Controller;
 use think\Session;
-use app\index\model\UserModel;
+
 class Base extends Controller {
 	protected $userinfo;
-	/**
-	 * [_initialize 中间件]
-	 * @return [type] [description]
-	 */
 	public function _initialize() {
 		$um = new UserModel();
-        $this->userinfo = $um->where('id',session('userid','','user_'))->find();
-		if(Session::has('username','user_') && Session::has('userid','user_')) {
-			return ;
+		if (cookie('username') != null && cookie('password') != null) {
+
+			$status = $um->where('username', cookie('username'))->where('password', md5(cookie('password')))->find();
+			if (!empty($status)) {
+				$userid = $status['id'];
+				$username = $status['username'];
+				$nickName = $status['nickName'];
+				$status['loginTime'] = time();
+				$status->save();
+				Session::set('userid', $userid, 'user_');
+				Session::set('username', $username, 'user_');
+				Session::set('nickName', $nickName, 'user_');
+			}
+
+		}
+		$this->userinfo = $um->where('id', session('userid', '', 'user_'))->find();
+		if (Session::has('username', 'user_') && Session::has('userid', 'user_')) {
+			return;
 		} else {
-			return $this->error('请先登录！',url('/tologin'),30);
+			return $this->error('请先登录！', url('/tologin'), 30);
+		}
 	}
-}
 }
 ?>
