f6fcba1c065a664ed9d447433cfac2b505bc84fc
ChinaGGW <36232080+ChinaGGW@users.noreply.github.com>
2018-05-04 09:48:38 +0800

    Update UserLogin.php

--- application/index/controller/UserLogin.php
+++ application/index/controller/UserLogin.php
@@ -1,9 +1,10 @@
 <?php
 namespace app\index\controller;
-use think\Controller;
 use app\index\model\UserModel;
+use think\Controller;
 use think\Request;
 use think\Session;
+
 class UserLogin extends Controller {
 	/**
 	 * [viewLogin 展示登陆界面]
@@ -19,7 +20,30 @@ class UserLogin extends Controller {
 	 */
 	public function toRegister(Request $request) {
 		$data = $request->post();
-		print_r($data);
+		//print_r($data);
+		if (!captcha_check($data['captcha'])) {
+			$msg = ['code' => '#captcha', 'msg' => '验证码不正确'];
+			return json_encode($msg);
+		};
+		$um = new UserModel();
+		$state = $um->where('username', $data['username'])->find();
+		if ($state) {
+			$msg = ['code' => '[name="username"]', 'msg' => '名称已被占用'];
+			return json_encode($msg);
+		}
+		unset($data['captcha']);
+		unset($data['repassword']);
+		$data['password'] = md5($data['pwd']);
+		unset($data['pwd']);
+		$um->save($data);
+		if ($um->id !== false) {
+			$msg = ['code' => '#zhucebtn', 'msg' => '注册成功！'];
+			return json_encode($msg);
+		} else {
+			$msg = ['code' => '#zhucebtn', 'msg' => '服务器错误'];
+			return json_encode($msg);
+		}
+
 	}
 	/**
 	 * [doLogin 登陆逻辑]
@@ -31,24 +55,28 @@ class UserLogin extends Controller {
 		$um = new UserModel();
 		$uname = $data['user'];
 		$password = md5($data['password']);
-		if($uname == '' || $password == '') {
-			$msg = ['code'=>0,'msg'=>'用户名或密码不能为空i！'];
+		if ($uname == '' || $password == '') {
+			$msg = ['code' => 0, 'msg' => '用户名或密码不能为空i！'];
 			return json_encode($msg);
 		}
-		$status = $um->where('username',$uname)->where('password',$password)->find();
-		if($status) {
-			$userid =  $status['id'];
+		$status = $um->where('username', $uname)->where('password', $password)->find();
+		if ($status) {
+			if ($data['rember']) {
+				setcookie('username', $uname, time() + 10000);
+				setcookie('password', $data['password'], time() + 10000);
+			}
+			$userid = $status['id'];
 			$username = $status['username'];
 			$nickName = $status['nickName'];
 			$status['loginTime'] = time();
 			$status->save();
-			Session::set('userid',$userid,'user_');  
-			Session::set('username',$username,'user_');
-			Session::set('nickName',$nickName,'user_');
-			$msg =  ['code'=>1,'msg'=>'success'];
+			Session::set('userid', $userid, 'user_');
+			Session::set('username', $username, 'user_');
+			Session::set('nickName', $nickName, 'user_');
+			$msg = ['code' => 1, 'msg' => 'success'];
 			return json_encode($msg);
 		} else {
-			$msg = ['code'=>0,'msg'=>'用户名或密码错误i！'];
+			$msg = ['code' => 0, 'msg' => '用户名或密码错误i！'];
 			return json_encode($msg);
 		}
 	}
@@ -57,8 +85,10 @@ class UserLogin extends Controller {
 	 * @return [type] [description]
 	 */
 	public function logOut() {
-		Session::flush();
-		return $this->error('退出成功！',url('/tologin'),30);
+		Session::clear('user_');
+		setcookie('username', '', time() - 1);
+		setcookie('password', '', time() - 1);
+		return $this->error('退出成功！', url('/tologin'), 30);
 	}
 }
 ?>
